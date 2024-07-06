4009499fb885a98b5798f0e094dbf439e75f1a38
koutyuke <gunji.kousuke08@gmail.com>
2024-07-06 23:43:08 +0900

    test: 🧪 (api/modules/cors) testの作成

---
+++ apps/api/src/modules/cors/test/credential.test.ts
@@ -0,0 +1,36 @@
+import { env } from "cloudflare:test";
+import { ElysiaWithEnv } from "@/modules/elysiaWithEnv";
+import { describe, expect, test } from "vitest";
+import { cors } from "../cors.plugin";
+
+describe("Credential Test", async () => {
+	test("falseの時にCredentialが設定されない", async () => {
+		const app = new ElysiaWithEnv({ aot: false })
+			.setEnv(env)
+			.use(
+				cors({
+					credentials: false,
+				}),
+			)
+			.get("/", () => "Test");
+
+		const res = await app.fetch(new Request("http://localhost/"));
+
+		expect(res.headers.get("Access-Control-Allow-Credentials")).toBeNull();
+	});
+
+	test("trueの時に全てのCredentialが許可される", async () => {
+		const app = new ElysiaWithEnv({ aot: false })
+			.setEnv(env)
+			.use(
+				cors({
+					credentials: true,
+				}),
+			)
+			.get("/", () => "Test");
+
+		const res = await app.fetch(new Request("http://localhost/", { headers: { Origin: "http://localhost" } }));
+
+		expect(res.headers.get("Access-Control-Allow-Credentials")).toBe("true");
+	});
+});
---
+++ apps/api/src/modules/cors/test/methods.test.ts
@@ -0,0 +1,53 @@
+import { env } from "cloudflare:test";
+import { ElysiaWithEnv } from "@/modules/elysiaWithEnv";
+import { describe, expect, test } from "vitest";
+import { cors } from "../cors.plugin";
+
+describe("Methods Test", async () => {
+	test("falseの時にMethodが設定されない", async () => {
+		const app = new ElysiaWithEnv({ aot: false })
+			.setEnv(env)
+			.use(
+				cors({
+					methods: false,
+				}),
+			)
+			.get("/", () => "Test");
+
+		const res = await app.fetch(new Request("http://localhost/"));
+
+		expect(res.headers.get("Access-Control-Allow-Methods")).toBeNull();
+	});
+
+	test("trueの時に全てのMethodが許可される", async () => {
+		const app = new ElysiaWithEnv({ aot: false })
+			.setEnv(env)
+			.use(
+				cors({
+					methods: true,
+				}),
+			)
+			.get("/", () => "Test");
+
+		const testCases = ["GET", "POST", "PUT", "DELETE", "PATCH"];
+
+		for (const testCase of testCases) {
+			const res = await app.fetch(new Request("http://localhost/", { method: testCase }));
+			expect(res.headers.get("Access-Control-Allow-Methods")).toBe(testCase);
+		}
+	});
+
+	test("指定したMethodのみ許可される", async () => {
+		const app = new ElysiaWithEnv({ aot: false })
+			.setEnv(env)
+			.use(
+				cors({
+					methods: ["GET", "POST"],
+				}),
+			)
+			.get("/", () => "Test");
+
+		const res = await app.fetch(new Request("http://localhost/"));
+		expect(res.headers.get("Access-Control-Allow-Methods")).toBe("GET, POST");
+	});
+});
