8fda84ed516c8e9aa2e860c803f3781ca3957cf9
koutyuke <gunji.kousuke08@gmail.com>
2024-07-09 22:46:54 +0900

    feat: ✨ (core) cookieの定数の定義

---
+++ packages/core/src/const/cookie.ts
@@ -0,0 +1,4 @@
+export const SESSION_COOKIE_NAME = "__mc_session";
+export const OAUTH_STATE_COOKIE_NAME = "__mc_oauth_state";
+export const OAUTH_CODE_VERIFIER_COOKIE_NAME = "__mc_oauth_code_verifier";
+export const OAUTH_REDIRECT_URI_COOKIE_NAME = "__mc_oauth_redirect_uri";
