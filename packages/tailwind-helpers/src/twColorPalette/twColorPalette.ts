4efa0279fdc75eadf3b71ce5c44ec49aeaf900a0
koutyuke <gunji.kousuke08@gmail.com>
2024-08-05 10:31:03 +0900

    feat: 🎈 (tailwind-helpers) create `twColorPalette`

---
+++ packages/tailwind-helpers/src/twColorPalette/twColorPalette.ts
@@ -0,0 +1,17 @@
+import { twMerge } from "tailwind-merge";
+import type { Color } from "../color.type";
+
+/**
+ * tailwindcss color palette
+ *
+ * tailwindcssで書かれたclassでcolorPaletteを指定された色に変換する関数
+ *
+ * @param { string } className
+ * @param { Color } color
+ * @returns { string }
+ */
+const twColorPalette = (className: string, color: Color): string => {
+	return twMerge(className.replace(/-colorPalette-/g, `-${color}-`));
+};
+
+export { twColorPalette };
