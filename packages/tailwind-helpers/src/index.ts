adefb5fbca75f967620d272e66aa29eb51bd873b
koutyuke <gunji.kousuke08@gmail.com>
2024-08-15 22:22:31 +0900

    feat: ⚰️ (tailwind-helpers) delete `libs` folder

--- packages/tailwind-helpers/src/index.ts
+++ packages/tailwind-helpers/src/index.ts
@@ -2,3 +2,5 @@ export { twPseudo } from "./twPseudo";
 export { twColorPalette } from "./twColorPalette";
 export type { Color, ResolveConfigTheme, ScaleColor, PureColor } from "./types/color.type";
 export type { PseudoClass } from "./types/pseudo-class.type";
+export * from "tailwind-merge";
+export * from "tailwind-variants";
--- packages/tailwind-helpers/tsup.config.ts
+++ packages/tailwind-helpers/tsup.config.ts
@@ -3,4 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["./src/babel/plugin.ts"],
 	outDir: "./dist",
+	external: ["@babel/core", "@babel/generator"],
+	dts: true,
 });
