06977509a3097ec4ebfa10d6a6f980a5af1035ec
koutyuke <gunji.kousuke08@gmail.com>
2024-08-24 16:33:10 +0900

    rename: 🚚 (tailwind-helpers) change camel case

--- packages/tailwind-helpers/src/index.ts
+++ packages/tailwind-helpers/src/index.ts
@@ -1,5 +1,5 @@
-export { twPseudo } from "./twPseudo";
-export { twColorPalette } from "./twColorPalette";
+export { twPseudo } from "./tw-pseudo";
+export { twColorPalette } from "./tw-color-palette";
 export type { Color, ResolveConfigTheme, ScaleColor, PureColor } from "./types/color.type";
 export type { PseudoClass } from "./types/pseudo-class.type";
 export * from "tailwind-merge";
