a45908c98a05ad4740614ecb516119b058323be1
koutyuke <gunji.kousuke08@gmail.com>
2024-09-03 17:46:02 +0900

    feat: ✨ (ui) create social components

---
+++ packages/ui/src/social/google/icon/icon.native.tsx
@@ -0,0 +1,20 @@
+import Icon from "@mona-ca/core/assets/social/google/icon.svg";
+import { twMerge } from "@mona-ca/tailwind-helpers";
+import { cssInterop } from "nativewind";
+import type { FC } from "react";
+
+type Props = {
+	className?: string;
+};
+
+cssInterop(Icon, {
+	className: {
+		target: "style",
+	},
+});
+
+const GoogleIcon: FC<Props> = ({ className }) => {
+	return <Icon className={twMerge("aspect-square h-full", className)} />;
+};
+
+export { GoogleIcon };
---
+++ packages/ui/src/social/index.native.ts
@@ -0,0 +1,2 @@
+export { DiscordIcon } from "./discord/index.native";
+export { GoogleIcon } from "./google/index.native";
