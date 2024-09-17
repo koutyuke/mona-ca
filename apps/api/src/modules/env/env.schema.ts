597edb1745c065cb367d2a6ee587520d08274311
koutyuke <gunji.kousuke08@gmail.com>
2024-09-17 13:47:30 +0900

    feat: 🎈 (api) add `RESEND_DEFAULT_EMAIL_API_KEY` env value

--- apps/api/src/modules/env/env.schema.ts
+++ apps/api/src/modules/env/env.schema.ts
@@ -9,6 +9,7 @@ export const OAuthProviderEnvSchema = t.Object({
 	DISCORD_CLIENT_SECRET: t.String(),
 	GOOGLE_CLIENT_ID: t.String(),
 	GOOGLE_CLIENT_SECRET: t.String(),
+	RESEND_DEFAULT_EMAIL_API_KEY: t.String(),
 });
 
 export const AppEnvWithoutCFModuleEnvSchema = t.Intersect([AppStatusEnvSchema, OAuthProviderEnvSchema]);
