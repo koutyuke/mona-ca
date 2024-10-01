23e812e2f1772be3ae5192e4a235945053991c3a
koutyuke <gunji.kousuke08@gmail.com>
2024-10-01 23:17:22 +0900

    feat: 🎈 (mobile) change wave svg file

--- apps/mobile/src/shared/ui/wave/wave.presenter.tsx
+++ apps/mobile/src/shared/ui/wave/wave.presenter.tsx
@@ -1,3 +1,4 @@
+import { twMerge } from "@mona-ca/tailwind-helpers";
 import { cssInterop } from "nativewind";
 import type { FC } from "react";
 import WaveSvg from "./wave.svg";
@@ -13,7 +14,7 @@ cssInterop(WaveSvg, {
 });
 
 const Wave: FC<Props> = ({ className }) => {
-	return <WaveSvg className={className ?? ""} />;
+	return <WaveSvg className={twMerge("aspect-[200_/_17] ", className)} />;
 };
 
 export { Wave };
