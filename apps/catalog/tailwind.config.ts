ac989c49e6ee80845bf8f328331a18ddfe89fa5f
koutyuke <gunji.kousuke08@gmail.com>
2024-10-08 22:32:28 +0900

    build: 📦️ (catalog) change setting

--- apps/catalog/tailwind.config.ts
+++ apps/catalog/tailwind.config.ts
@@ -16,6 +16,10 @@ const config = {
 		transform: twHelperTransforms,
 	},
 	presets: [nativewindConfig, baseConfig],
+	corePlugin: {
+		textOpacity: true,
+		backgroundOpacity: true,
+	},
 } satisfies Config;
 
 export default config;
