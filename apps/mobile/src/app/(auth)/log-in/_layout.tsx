36c998e0b6cbd9de058edfeceffe84a77240653a
koutyuke <gunji.kousuke08@gmail.com>
2024-10-20 15:31:36 +0900

    feat: ✨ (mobile/apps) create log-in screen

---
+++ apps/mobile/src/app/(auth)/log-in/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+
+const LogInLayout = () => {
+	return (
+		<Stack
+			screenOptions={{
+				headerTransparent: true,
+			}}
+		/>
+	);
+};
+
+export default LogInLayout;
