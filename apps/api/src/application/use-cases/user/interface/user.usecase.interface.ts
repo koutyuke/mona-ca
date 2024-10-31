4eafa65d3ae9cddb094067e2bcc797b879c43274
koutyuke <gunji.kousuke08@gmail.com>
2024-11-01 02:45:56 +0900

    🎈 feat(api/usecase): update user usecase

--- apps/api/src/application/use-cases/user/interface/user.usecase.interface.ts
+++ apps/api/src/application/use-cases/user/interface/user.usecase.interface.ts
@@ -1,20 +1,21 @@
 import type { Session } from "@/domain/session";
 import type { User } from "@/domain/user";
-import type { UserCredentials } from "@/domain/user-credentials";
+import type { UserCredential } from "@/domain/user-credential";
 
 export interface IUserUseCase {
 	getUser(id: User["id"]): Promise<User | null>;
 	getUserByEmail(email: User["email"]): Promise<User | null>;
 	getUserBySessionId(sessionId: Session["id"]): Promise<User | null>;
 	createUser(
-		user: Omit<ConstructorParameters<typeof User>[0], "id" | "createdAt" | "updatedAt"> &
-			Partial<Pick<ConstructorParameters<typeof User>[0], "id">> &
-			Partial<Omit<ConstructorParameters<typeof UserCredentials>[0], "userId">>,
-	): Promise<User>;
+		user: Omit<ConstructorParameters<typeof User>[0], "id" | "createdAt" | "updatedAt">,
+		options?: {
+			id?: ConstructorParameters<typeof User>[0]["id"];
+			credential?: Partial<Omit<ConstructorParameters<typeof UserCredential>[0], "userId" | "createdAt" | "updatedAt">>;
+		},
+	): Promise<{ user: User; userCredential: UserCredential }>;
 	updateUser(
 		id: User["id"],
 		user: Partial<Omit<ConstructorParameters<typeof User>[0], "id" | "updatedAt" | "createdAt">>,
 	): Promise<User>;
 	deleteUser(id: User["id"]): Promise<void>;
-	generateId(): string;
 }
