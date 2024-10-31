14e2017fde13dabf0ab7c09dc07681c072a3c2f0
koutyuke <gunji.kousuke08@gmail.com>
2024-11-01 02:46:52 +0900

    🎈 feat(api/usecase): update auth usecase

--- apps/api/src/application/use-cases/auth/interface/auth.usecase.interface.ts
+++ apps/api/src/application/use-cases/auth/interface/auth.usecase.interface.ts
@@ -4,21 +4,32 @@ import type { User } from "@/domain/user";
 import type { Cookie as ElysiaCookie } from "elysia";
 
 export interface IAuthUseCase {
-	createBlankSessionCookie(): Cookie;
-	createSession(userId: User["id"]): Promise<Session>;
-	createSessionCookie(sessionId: Session["id"]): Cookie;
-	deleteExpiredSessions(): Promise<void>;
+	// Session Token
+	hashToken(token: string): string;
+	generateSessionToken(): string;
+	validateSessionToken(token: string): Promise<{
+		session: Session;
+		user: User;
+	} | null>;
+
+	// Session
+	createSession(token: string, userId: User["id"]): Promise<Session>;
 	getUserSessions(userId: User["id"]): Promise<Session[]>;
+	deleteExpiredSessions(): Promise<void>;
 	invalidateSession(sessionId: Session["id"]): Promise<void>;
 	invalidateUserSessions(userId: User["id"]): Promise<void>;
+
+	// Cookie
+	createSessionCookie(token: string): Cookie;
+	createBlankSessionCookie(): Cookie;
+
+	// Password
+	hashPassword(password: string): Promise<string>;
+	verifyPasswordHash(password: string, passwordHash: string): Promise<boolean>;
+
+	// Utility
 	readBearerToken(authorizationHeader: string): string | null;
 	readSessionCookie(cookieHeader: {
 		[key: string]: ElysiaCookie<string | undefined>;
 	}): string | null;
-	validateSession(sessionId: Session["id"]): Promise<{
-		session: Session;
-		user: User;
-	} | null>;
-	hashedPassword(password: string): Promise<string>;
-	verifyHashedPassword(password: string, hashedPassword: string): Promise<boolean>;
 }
