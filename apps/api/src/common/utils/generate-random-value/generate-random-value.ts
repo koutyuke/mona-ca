33eed30111d89a126d20ce0ac16335afdd4cdaa8
koutyuke <gunji.kousuke08@gmail.com>
2024-11-09 23:02:08 +0900

    🎈 feat(api/utils): enhance random string generation with customizable character sets

--- apps/api/src/common/utils/generate-random-value/generate-random-value.ts
+++ apps/api/src/common/utils/generate-random-value/generate-random-value.ts
@@ -6,7 +6,9 @@ import {
 	generateRandomString as osloGenerateRandomString,
 } from "@oslojs/crypto/random";
 
-const alphabetCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseAlphabetCharacters = "abcdefghijklmnopqrstuvwxyz";
+
+const uppercaseAlphabetCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const numberCharacters = "0123456789";
 
@@ -16,20 +18,52 @@ export const random: RandomReader = {
 	},
 };
 
+type Character = "lowercaseAlphabet" | "uppercaseAlphabet" | "number";
+
+/**
+ * Generates a random string of the specified length using the provided options.
+ *
+ * @param length - The length of the random string to generate.
+ * @param options - An optional object to specify the character set to use. If you don't provide any options, the generated string will include lowercase alphabetic characters and numbers. But if you provide any of the options, the generated string will only include the specified characters.
+ * @param options.uppercaseAlphabet - If true, the generated string will include uppercase alphabetic characters.
+ * @param options.lowercaseAlphabet - If true, the generated string will include lowercase alphabetic characters.
+ * @param options.numbers - If true, the generated string will include numeric characters.
+ * @returns A random string of the specified length.
+ * @throws Will throw an error if none of the options are set to true.
+ */
 export const generateRandomString = (
 	length: number,
 	options?: {
-		alphabet?: boolean;
-		numbers?: boolean;
-	},
+		[key in Character]: Record<key, true> & Omit<Partial<Record<Character, boolean>>, key>;
+	}[Character],
 ): string => {
-	const { alphabet = true, numbers = true } = options ?? {};
+	const {
+		lowercaseAlphabet = false,
+		uppercaseAlphabet = false,
+		number = false,
+	} = options ?? {
+		lowercaseAlphabet: true,
+		uppercaseAlphabet: true,
+		number: true,
+	};
 
-	const characters = (alphabet ? alphabetCharacters : "") + (numbers ? numberCharacters : "");
+	const characters = [
+		lowercaseAlphabet && lowercaseAlphabetCharacters,
+		uppercaseAlphabet && uppercaseAlphabetCharacters,
+		number && numberCharacters,
+	]
+		.filter(Boolean)
+		.join("");
 
 	return osloGenerateRandomString(random, characters, length);
 };
 
-export const generateRandomIntegerNumber = (length: number): number => {
-	return osloGenerateRandomIntegerNumber(random, length);
+/**
+ * Generates a random integer number with the specified length.
+ *
+ * @param max - The maximum value of the random number to generate.
+ * @returns A random integer number with the specified length.
+ */
+export const generateRandomIntegerNumber = (max: number): number => {
+	return osloGenerateRandomIntegerNumber(random, max);
 };
