ccb679b5f939ffd3a9324ccde7ba6348ec288d32
koutyuke <gunji.kousuke08@gmail.com>
2025-02-03 10:01:26 +0900

    🪦 feat(packages/tailwind-helpers): remove twColorPalette

--- packages/tailwind-helpers/src/index.ts
+++ packages/tailwind-helpers/src/index.ts
@@ -1,5 +1,4 @@
 export { twPseudo } from "./tw-pseudo";
-export { twColorPalette } from "./tw-color-palette";
 export type { Color, ResolveConfigTheme, ScaleColor, PureColor } from "./types/color.type";
 export type { PseudoClass } from "./types/pseudo-class.type";
 export * from "tailwind-merge";
--- packages/tailwind-helpers/src/tw-color-palette/index.ts
+++
@@ -1,1 +0,0 @@
-export { twColorPalette } from "./tw-color-palette";
