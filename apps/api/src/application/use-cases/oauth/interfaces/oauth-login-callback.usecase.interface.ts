87a24f49dcc72fd16c2ea6f68715810c8db20b30
koutyuke <gunji.kousuke08@gmail.com>
2025-02-22 22:08:53 +0900

    🎈 feat(api): refactor OAuth login callback to use Result type for error handling

--- apps/api/src/application/use-cases/oauth/interfaces/oauth-login-callback.usecase.interface.ts
+++ apps/api/src/application/use-cases/oauth/interfaces/oauth-login-callback.usecase.interface.ts
@@ -1,11 +1,21 @@
+import type { Err, Result } from "../../../../common/utils";
 import type { OAuthProvider } from "../../../../domain/entities/oauth-account";
 import type { Session } from "../../../../domain/entities/session";
 
-export interface IOAuthLoginCallbackUseCaseResult {
+export type OAuthLoginCallbackUseCaseSuccessResult = {
 	session: Session;
 	sessionToken: string;
-}
+};
 
+export type OAuthLoginCallbackUseCaseErrorResult =
+	| Err<"FAILED_TO_GET_ACCOUNT_INFO">
+	| Err<"OAUTH_ACCOUNT_NOT_FOUND">
+	| Err<"OAUTH_ACCOUNT_NOT_FOUND_BUT_LINKABLE">;
+
+export type OAuthLoginCallbackUseCaseResult = Result<
+	OAuthLoginCallbackUseCaseSuccessResult,
+	OAuthLoginCallbackUseCaseErrorResult
+>;
 export interface IOAuthLoginCallbackUseCase {
-	execute(provider: OAuthProvider, code: string, codeVerifier: string): Promise<IOAuthLoginCallbackUseCaseResult>;
+	execute(provider: OAuthProvider, code: string, codeVerifier: string): Promise<OAuthLoginCallbackUseCaseResult>;
 }
