276b179d96b421cfa688c4d03991a5162c13fb6c
koutyuke <gunji.kousuke08@gmail.com>
2025-03-09 06:18:38 +0900

    ♻️ refactor(api/use-case)!: update email verification repository and improve change email logic

--- apps/api/src/application/use-cases/user/change-email.usecase.ts
+++ apps/api/src/application/use-cases/user/change-email.usecase.ts
@@ -1,34 +1,38 @@
-import type { User } from "../../../domain/entities";
-import type { IEmailVerificationCodeRepository } from "../../../interface-adapter/repositories/email-verification-code";
+import { User } from "../../../domain/entities";
+import type { IEmailVerificationRepository } from "../../../interface-adapter/repositories/email-verification";
 import type { IUserRepository } from "../../../interface-adapter/repositories/user";
 import type { IChangeEmailUseCase, IChangeEmailUseCaseResult } from "./interfaces/change-email.usecase.interface";
 
 export class ChangeEmailUseCase implements IChangeEmailUseCase {
 	constructor(
 		private userRepository: IUserRepository,
-		private emailVerificationCodeRepository: IEmailVerificationCodeRepository,
+		private emailVerificationRepository: IEmailVerificationRepository,
 	) {}
 
 	public async execute(email: string, code: string, user: User): Promise<IChangeEmailUseCaseResult> {
 		const [sameEmailUser, databaseCode] = await Promise.all([
 			this.userRepository.findByEmail(email),
-			this.emailVerificationCodeRepository.findByUserId(user.id),
+			this.emailVerificationRepository.findByUserId(user.id),
 		]);
 
 		if (sameEmailUser || !databaseCode || databaseCode.code !== code) {
 			return { success: false };
 		}
 
-		await this.emailVerificationCodeRepository.delete({ userId: user.id });
+		await this.emailVerificationRepository.deleteByUserId(user.id);
 
 		if (databaseCode.isExpired || databaseCode.email === user.email) {
 			return { success: false };
 		}
 
-		await this.userRepository.update(user.id, {
-			email,
+		const updatedUser = new User({
+			...user,
 			emailVerified: true,
+			email,
+			updatedAt: new Date(),
 		});
+
+		await this.userRepository.save(updatedUser);
 
 		return { success: true };
 	}
