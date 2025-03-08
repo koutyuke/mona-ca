520c4a0dd4e29a2ef8507ee78c3eb0d2eee91b59
koutyuke <gunji.kousuke08@gmail.com>
2025-03-09 06:09:47 +0900

    🎈 feat(api/domain/entities): remove 'fresh' property and add 'shouldRefreshExpiration' method in Session class

--- apps/api/src/domain/entities/session.ts
+++ apps/api/src/domain/entities/session.ts
@@ -1,24 +1,26 @@
+import { sessionRefreshSpan } from "../../common/constants";
 import type { SessionId, UserId } from "../value-object";
 
 export class Session {
 	readonly id: SessionId;
 	readonly userId: UserId;
 	readonly expiresAt: Date;
-	readonly fresh: boolean;
 
 	constructor(args: {
 		id: SessionId;
 		userId: UserId;
 		expiresAt: Date;
-		fresh?: boolean;
 	}) {
 		this.id = args.id;
 		this.userId = args.userId;
 		this.expiresAt = args.expiresAt;
-		this.fresh = !!args.fresh;
 	}
 
 	get isExpired() {
 		return Date.now() >= this.expiresAt.getTime();
+	}
+
+	get shouldRefreshExpiration() {
+		return Date.now() >= this.expiresAt.getTime() - sessionRefreshSpan.milliseconds();
 	}
 }
