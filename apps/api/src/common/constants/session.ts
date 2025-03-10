34135750ff1527c5f789c777c9887f5cda733c4d
koutyuke <gunji.kousuke08@gmail.com>
2025-03-10 22:09:42 +0900

    🎈 feat(api/common/constants): remove email verification constants and add session constants

--- apps/api/src/common/constants/session.ts
+++ apps/api/src/common/constants/session.ts
@@ -5,3 +5,7 @@ export const SESSION_REFRESH_SPAN_DAYS = 15 as const;
 
 export const sessionExpiresSpan = new TimeSpan(SESSION_EXPIRES_SPAN_DAYS, "d");
 export const sessionRefreshSpan = new TimeSpan(SESSION_REFRESH_SPAN_DAYS, "d");
+
+export const EMAIL_VERIFICATION_SESSION_EXPIRES_SPAN_MINUTES = 15 as const;
+
+export const emailVerificationSessionExpiresSpan = new TimeSpan(EMAIL_VERIFICATION_SESSION_EXPIRES_SPAN_MINUTES, "m");
