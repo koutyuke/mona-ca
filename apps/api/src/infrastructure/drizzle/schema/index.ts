4c83e873981da29ccf571512c950756390d59e79
koutyuke <gunji.kousuke08@gmail.com>
2025-03-30 21:45:03 +0900

    ✨ feat(api/schema): add password reset session schema and export it

--- apps/api/src/infrastructure/drizzle/schema/index.ts
+++ apps/api/src/infrastructure/drizzle/schema/index.ts
@@ -2,3 +2,4 @@ export { oauthAccounts } from "./oauth-accounts";
 export { sessions } from "./sessions";
 export { users } from "./users";
 export { emailVerificationSessions } from "./email-verification-sessions";
+export { passwordResetSessions } from "./password-reset-session";
