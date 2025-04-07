c2765bf7fc1c81fc2ba6e4c80be71de320bcb8e1
koutyuke <gunji.kousuke08@gmail.com>
2025-04-07 23:45:26 +0900

    🎈 feat(api/interface-adapter/gateway/oauth-provider): update parameter names for consistency and improve clarity

--- apps/api/src/interface-adapter/gateway/oauth-provider/interfaces/oauth-provider.gateway.interface.ts
+++ apps/api/src/interface-adapter/gateway/oauth-provider/interfaces/oauth-provider.gateway.interface.ts
@@ -4,12 +4,12 @@ export type AccountInfo = {
 	id: string;
 	email: string;
 	name: string;
-	iconUrl: string | null;
+	iconURL: string | null;
 	emailVerified: boolean;
 };
 
 export interface IOAuthProviderGateway {
-	genAuthUrl(state: string, codeVerifier: string): URL;
+	genAuthURL(state: string, codeVerifier: string): URL;
 	getTokens(code: string, codeVerifier: string): Promise<OAuth2Tokens>;
 	getAccountInfo(accessToken: string): Promise<AccountInfo | null>;
 	revokeToken(token: string): Promise<void>;
