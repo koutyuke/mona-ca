4111ae83a1a95498ceb5aa055e49fd7aef230f89
koutyuke <gunji.kousuke08@gmail.com>
2025-04-10 15:29:00 +0900

    🎈 feat(api/common/schema): enhance StringEnum implementation and add custom error handling

--- apps/api/src/common/schema.ts
+++ apps/api/src/common/schema.ts
@@ -1,15 +1,42 @@
-import { type TUnion, Type, TypeGuard } from "@sinclair/typebox";
-import { type TSchema, t } from "elysia";
+import { Kind, type SchemaOptions, type TSchema, type TUnion, Type, TypeGuard, TypeRegistry } from "@sinclair/typebox";
+import { DefaultErrorFunction, SetErrorFunction, ValueErrorType } from "@sinclair/typebox/errors";
 
-export const StringEnum = <T extends string[]>(values: [...T]) =>
-	t.Unsafe<T[number]>({
+TypeRegistry.Set("StringEnum", (schema: { enum: string[] }, value: unknown) => {
+	return typeof value === "string" && schema.enum.includes(value);
+});
+
+SetErrorFunction(error => {
+	if (error.errorType === ValueErrorType.Kind) {
+		if (
+			error.schema.type === "string" &&
+			typeof error.value === "string" &&
+			error.schema.enum &&
+			!error.schema.enum.includes(error.value)
+		) {
+			return `Must include valid value: [${error.schema.enum.join(", ")}]`;
+		}
+	}
+
+	return DefaultErrorFunction(error);
+});
+
+// create a type method for string enum
+export const StringEnum = <T extends string[]>(values: [...T], options: SchemaOptions = {}) => {
+	return Type.Unsafe<T[number]>({
+		...options,
+		[Kind]: "StringEnum",
 		type: "string",
 		enum: values,
 	});
-
-export const Nullable = <T extends TSchema>(T: T) => {
-	return t.Union([T, t.Null()]);
 };
+
+// export const StringEnum = <T extends string[]>(values: [...T]) => {
+// 	return Type.Union(values.map(str => Type.Literal(str)));
+// };
+
+// export const Nullable = <T extends TSchema>(T: T) => {
+// 	return Type.Union([T, Type.Null()]);
+// };
 
 // This function flattens a union of unions into a single union.
 // Warning: This function is not type-safe.
