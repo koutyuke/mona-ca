9d67c90e54749078bcb31547ec43f8df1bdc6fd9
koutyuke <gunji.kousuke08@gmail.com>
2025-04-11 14:55:14 +0900

    🎈 feat(api/open-api/path-detail): enhance authentication description with client type header handling

--- apps/api/src/modules/open-api/path-detail.ts
+++ apps/api/src/modules/open-api/path-detail.ts
@@ -1,5 +1,5 @@
 import type { DocumentDecoration } from "elysia";
-import { SESSION_COOKIE_NAME } from "../../common/constants";
+import { CLIENT_TYPE_HEADER_NAME, SESSION_COOKIE_NAME } from "../../common/constants";
 import type { Tag } from "./tag";
 
 type PathDocumentProps = {
@@ -14,25 +14,25 @@ type PathDocumentProps = {
 export const pathDetail = (props: PathDocumentProps): DocumentDecoration => {
 	let { operationId, summary, description = "", tag, withAuth = false, hide = false } = props;
 
-	if (Array.isArray(description)) {
-		description = description.join("\n\n");
+	if (typeof description === "string") {
+		description = [description];
 	}
 
 	if (withAuth) {
-		description = description ? `${description}\n\n` : "";
-		description =
-			description +
-			[
-				"##### **Authentication**",
-				"---",
-				"1. **Bearer Authentication**",
-				"   The bearer token to authenticate the request",
-				"---",
-				"2. **Cookie Authentication**",
-				"   The session cookie to authenticate the request",
-				`   _in: cookie, key: \`${SESSION_COOKIE_NAME}\`_`,
-			].join("\n\n");
+		description = [
+			...description,
+			"##### **Authentication**",
+			"---",
+			"1. **Bearer Authentication**",
+			`   If the \`${CLIENT_TYPE_HEADER_NAME}\` header is \`mobile\`, the bearer token to authenticate the request`,
+			"---",
+			"2. **Cookie Authentication**",
+			`   If the \`${CLIENT_TYPE_HEADER_NAME}\` header is \`web\`, the session cookie to authenticate the request`,
+			`   _in: cookie, key: \`${SESSION_COOKIE_NAME}\`_`,
+		];
 	}
+
+	description = description.join("\n\n");
 
 	return {
 		operationId,
