569f9784275f7574736c8429f841db1aa43fd9d1
koutyuke <gunji.kousuke08@gmail.com>
2025-04-12 17:10:59 +0900

    🎈 feat(api/elysia-with-env/response): refactor NoContentResponse to be a function returning a Response object

--- apps/api/src/modules/elysia-with-env/response.ts
+++ apps/api/src/modules/elysia-with-env/response.ts
@@ -1,5 +1,5 @@
 import { t } from "elysia";
 
-export const NoContentResponse = new Response(null, { status: 204 }) as unknown as null;
+export const NoContentResponse = () => new Response(null, { status: 204 }) as unknown as null;
 
 export const NoContentResponseSchema = t.Null();
