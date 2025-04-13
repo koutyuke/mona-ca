a1a1591ce8d253f5c87dd31358ea1f1f4ab7d7b4
koutyuke <gunji.kousuke08@gmail.com>
2025-04-13 10:58:12 +0900

    🎈 feat(api/application/use-cases/user): refactor update user profile use case to utilize updateUser function

--- apps/api/src/application/use-cases/user/update-user-profile.usecase.ts
+++ apps/api/src/application/use-cases/user/update-user-profile.usecase.ts
@@ -1,4 +1,5 @@
-import { User } from "../../../domain/entities";
+import { updateUser } from "../../../domain/entities";
+import type { User } from "../../../domain/entities";
 import type { IUserRepository } from "../../../interface-adapter/repositories/user";
 import type {
 	IUpdateUserProfileUseCase,
@@ -10,11 +11,7 @@ export class UpdateUserProfileUseCase implements IUpdateUserProfileUseCase {
 	constructor(private readonly userRepository: IUserRepository) {}
 
 	public async execute(currentUser: User, dto: UpdateUserProfileDto): Promise<UpdateUserProfileUseCaseResult> {
-		const updatedUser = new User({
-			...currentUser,
-			...dto,
-			updatedAt: new Date(),
-		});
+		const updatedUser = updateUser(currentUser, dto);
 
 		await this.userRepository.save(updatedUser);
 
