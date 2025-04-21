a71e856402808d80c394a6f3dbdfc044a811dd57
koutyuke <gunji.kousuke08@gmail.com>
2025-04-21 12:01:17 +0900

    🎈 feat(api/modules/elysia-with-env/response): add RedirectResponse and RedirectResponseSchema for handling redirects

--- apps/api/src/modules/elysia-with-env/response.ts
+++ apps/api/src/modules/elysia-with-env/response.ts
@@ -3,3 +3,8 @@ import { t } from "elysia";
 export const NoContentResponse = () => new Response(null, { status: 204 }) as unknown as null;
 
 export const NoContentResponseSchema = t.Null();
+
+export const RedirectResponse = (url: string, status: 302 | 301 | 303 | 307 | 308 = 302) =>
+	new Response(null, { status, headers: { location: url } }) as unknown as null;
+
+export const RedirectResponseSchema = t.Null();
