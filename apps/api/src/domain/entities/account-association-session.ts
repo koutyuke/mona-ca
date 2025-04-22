f055b8612af7fa09a130341231320eacff78a55e
koutyuke <gunji.kousuke08@gmail.com>
2025-04-22 14:31:42 +0900

    ✨ feat(api/domain/entities/account-association-session): add AccountAssociationSession interface and related functions

---
+++ apps/api/src/domain/entities/account-association-session.ts
@@ -0,0 +1,38 @@
+import { accountAssociationSessionExpiresSpan } from "../../common/constants";
+import type { OAuthProvider, OAuthProviderId, UserId } from "../value-object";
+import type { AccountAssociationSessionId } from "../value-object";
+
+export interface AccountAssociationSession {
+	id: AccountAssociationSessionId;
+	userId: UserId;
+	code: string;
+	email: string;
+	emailVerified: boolean;
+	provider: OAuthProvider;
+	providerId: OAuthProviderId;
+	expiresAt: Date;
+}
+
+export const createAccountAssociationSession = (args: {
+	id: AccountAssociationSessionId;
+	userId: UserId;
+	code: string;
+	email: string;
+	provider: OAuthProvider;
+	providerId: OAuthProviderId;
+}): AccountAssociationSession => {
+	return {
+		id: args.id,
+		userId: args.userId,
+		code: args.code,
+		email: args.email,
+		emailVerified: false,
+		provider: args.provider,
+		providerId: args.providerId,
+		expiresAt: new Date(Date.now() + accountAssociationSessionExpiresSpan.milliseconds()),
+	};
+};
+
+export const isExpiredAccountAssociationSession = (session: AccountAssociationSession): boolean => {
+	return session.expiresAt.getTime() < Date.now();
+};
