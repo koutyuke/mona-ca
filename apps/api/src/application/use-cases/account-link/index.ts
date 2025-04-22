0b7187e51f9a7b29bdcdba1a75dd0b7cf432d43c
koutyuke <gunji.kousuke08@gmail.com>
2025-04-22 14:35:04 +0900

    ✨ feat(api/application/use-cases/account-link/utils): add utility functions for account association state management

--- apps/api/src/application/use-cases/account-link/index.ts
+++ apps/api/src/application/use-cases/account-link/index.ts
@@ -1,6 +1,7 @@
 export { AccountLinkCallbackUseCase } from "./account-link-callback.usecase";
 export { AccountLinkRequestUseCase } from "./account-link-request.usecase";
 export { UnlinkAccountConnectionUseCase } from "./unlink-account-connection.usecase";
+export { validateAccountAssociationState, generateAccountAssociationState } from "./utils";
 
 export type { IAccountLinkCallbackUseCase } from "./interfaces/account-link-callback.usecase.interface";
 export type { IAccountLinkRequestUseCase } from "./interfaces/account-link-request.usecase.interface";
