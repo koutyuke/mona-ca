825aa006d178a0ca1aedbb28bb4df8eef60fd022
koutyuke <gunji.kousuke08@gmail.com>
2025-04-24 11:18:38 +0900

    🎈 feat(api/application/use-cases/password): add rate limiting for password reset email verification

--- apps/api/src/application/use-cases/password/password-reset-verify-email.usecase.ts
+++ apps/api/src/application/use-cases/password/password-reset-verify-email.usecase.ts
@@ -12,6 +12,7 @@ export class PasswordResetVerifyEmailUseCase implements IPasswordResetVerifyEmai
 	constructor(
 		private readonly passwordResetSessionRepository: IPasswordResetSessionRepository,
 		private readonly passwordResetSessionTokenService: ISessionTokenService,
+		private readonly passwordResetSessionRateLimit: (sessionId: string) => Promise<void>,
 	) {}
 
 	public async execute(
@@ -21,7 +22,11 @@ export class PasswordResetVerifyEmailUseCase implements IPasswordResetVerifyEmai
 		const passwordResetSessionId = newPasswordResetSessionId(
 			this.passwordResetSessionTokenService.hashSessionToken(passwordResetSessionToken),
 		);
-		const passwordResetSession = await this.passwordResetSessionRepository.findById(passwordResetSessionId);
+
+		const [passwordResetSession, _] = await Promise.all([
+			this.passwordResetSessionRepository.findById(passwordResetSessionId),
+			this.passwordResetSessionRateLimit(passwordResetSessionId),
+		]);
 
 		if (passwordResetSession === null) {
 			return err("INVALID_TOKEN");
