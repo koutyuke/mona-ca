3f9b035e6c6f18398bfbd3bdfbda81d0f16f314f
koutyuke <gunji.kousuke08@gmail.com>
2025-06-03 11:03:39 +0900

    🎈 feat(ui/components/text-input): update TextInput component with new styles, improved rendering, and additional props for better customization

--- packages/ui/src/components/text-input/text-input.native.story.tsx
+++ packages/ui/src/components/text-input/text-input.native.story.tsx
@@ -4,29 +4,13 @@ import { EmailIcon } from "../../icons/index.native";
 import { TextInput } from "./text-input.native";
 
 const meta: Meta<typeof TextInput> = {
+	title: "components/TextInput",
 	component: TextInput,
-};
-
-export default meta;
-
-type Story = StoryObj<typeof TextInput>;
-
-const Template: Story = {
-	render: args => (
-		<View className="w-full p-4">
-			<TextInput {...args} />
-		</View>
-	),
 	argTypes: {
 		size: {
 			options: ["sm", "md"],
 			control: {
 				type: "radio",
-			},
-		},
-		label: {
-			control: {
-				type: "text",
 			},
 		},
 		placeholder: {
@@ -44,11 +28,6 @@ const Template: Story = {
 				type: "boolean",
 			},
 		},
-		readOnly: {
-			control: {
-				type: "boolean",
-			},
-		},
 		error: {
 			control: {
 				type: "boolean",
@@ -57,11 +36,24 @@ const Template: Story = {
 	},
 };
 
+export default meta;
+
+type Story = StoryObj<typeof TextInput>;
+
 export const Default: Story = {
 	args: {
-		label: "Email",
 		placeholder: "Enter your email",
 		icon: EmailIcon,
 	},
-	...Template,
+	render: args => (
+		<View className="flex w-full flex-col gap-4 p-4">
+			<TextInput value="我々は宇宙人である。 I am an alien." size="md" />
+			<TextInput value="我々は宇宙人である。 I am an alien." size="sm" />
+			<TextInput {...args} value="我々は宇宙人である。 I am an alien." size="md" />
+			<TextInput {...args} value="我々は宇宙人である。 I am an alien." size="md" error />
+			<TextInput {...args} value="我々は宇宙人である。 I am an alien." size="md" disabled />
+			<TextInput {...args} value="我々は宇宙人である。 I am an alien." size="md" credentials />
+			<TextInput {...args} value="我々は宇宙人である。 I am an alien." size="sm" />
+		</View>
+	),
 };
