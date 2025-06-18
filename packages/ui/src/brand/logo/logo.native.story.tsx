e3eb574abfa64a9d58244c709f641fc989468338
koutyuke <gunji.kousuke08@gmail.com>
2025-06-19 00:29:03 +0900

    🎈 feat(ui/brand/logo): add story for MonaCaLogo component and update render method

--- packages/ui/src/brand/logo/logo.native.story.tsx
+++ packages/ui/src/brand/logo/logo.native.story.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { MonaCaLogo } from "./logo.native";
 
 const meta: Meta<typeof MonaCaLogo> = {
+	title: "brand/Logo",
 	component: MonaCaLogo,
 };
 
@@ -9,13 +10,9 @@ export default meta;
 
 type Story = StoryObj<typeof MonaCaLogo>;
 
-const Template: Story = {
-	render: args => <MonaCaLogo {...args} />,
-};
-
 export const Default: Story = {
 	args: {
 		className: "h-16",
 	},
-	...Template,
+	render: args => <MonaCaLogo {...args} />,
 };
