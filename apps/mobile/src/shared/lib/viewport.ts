2a3594b8ad6b47f866757adefc9126143d7b805c
koutyuke <gunji.kousuke08@gmail.com>
2025-06-21 17:02:45 +0900

    🎈 feat(mobile/shared/lib): update vw and vh functions to accept parameters for dynamic calculations

--- apps/mobile/src/shared/lib/viewport.ts
+++ apps/mobile/src/shared/lib/viewport.ts
@@ -3,5 +3,5 @@ import { Dimensions } from "react-native";
 export const screenHeight = Dimensions.get("screen").height;
 export const screenWidth = Dimensions.get("screen").width;
 
-export const vw = screenWidth / 100;
-export const vh = screenHeight / 100;
+export const vw = (width: number) => (screenWidth / 100) * width;
+export const vh = (height: number) => (screenHeight / 100) * height;
