6ff170c841c25b80323779f0c7527cecbfbd6b6b
koutyuke <gunji.kousuke08@gmail.com>
2025-06-21 17:03:10 +0900

    🪦 feat(mobile/widgets/layout): remove PageWaveTitle component and add WaveHeader component with story

--- apps/mobile/src/widgets/layout/index.ts
+++ apps/mobile/src/widgets/layout/index.ts
@@ -1,4 +1,5 @@
 export { Header } from "./ui/header";
-export { PageWaveTitle } from "./ui/page-wave-title";
 export { calculateHeaderAnimationTriggerPointOfPageWaveTitle } from "./utils";
 export { Body, ScrollBody } from "./ui/body";
+
+export { WaveHeader } from "./ui/header/wave-header";
