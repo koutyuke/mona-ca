165d88f4df4d0600dfece79f8fae8cf12b9a3131
koutyuke <gunji.kousuke08@gmail.com>
2025-06-21 17:01:17 +0900

    🎈 feat(mobile/app): update app.json for improved iOS support and add splash screen configuration

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -7,13 +7,10 @@ module.exports = api => {
 				"module-resolver",
 				{
 					alias: {
-						// tsconfig.json paths
 						"@mobile/assets": "../mobile/assets",
 						"@mobile": "../mobile/src",
 						"@ui/assets": "../../packages/ui/assets",
 						"@ui": "../../packages/ui/src",
-						// module exports fields
-						// "@mona-ca/ui/native": "../../packages/ui/src/index.native.ts",
 					},
 				},
 			],
