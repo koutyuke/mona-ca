73a5dea2a53317e1b1ae3ad40b2ce72feaf4a724
koutyuke <gunji.kousuke08@gmail.com>
2025-07-04 19:05:59 +0900

    ♻️ refactor(api/application/use-cases/user): remove unused UpdateUserProfileUseCaseResult type from user profile use case

--- apps/api/src/application/use-cases/user/index.ts
+++ apps/api/src/application/use-cases/user/index.ts
@@ -3,5 +3,4 @@ export { UpdateUserProfileUseCase } from "./update-user-profile.usecase";
 export type {
 	IUpdateUserProfileUseCase,
 	UpdateUserProfileDto,
-	UpdateUserProfileUseCaseResult,
 } from "./interfaces/update-user-profile.usecase.interface";
