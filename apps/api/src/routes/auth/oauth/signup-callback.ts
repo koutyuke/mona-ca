c8e8fe07fcf7a5d53ca766b380e00afd93d8da17
koutyuke <gunji.kousuke08@gmail.com>
2025-07-06 00:54:44 +0900

    ♻️ refactor(api/routes/auth/oauth): enhance error handling and response types in OAuth login and signup routes

--- apps/api/src/routes/auth/oauth/signup-callback.ts
+++ apps/api/src/routes/auth/oauth/signup-callback.ts
@@ -9,8 +9,8 @@ import {
 	OAUTH_STATE_COOKIE_NAME,
 	SESSION_COOKIE_NAME,
 } from "../../../common/constants";
-import { FlattenUnion } from "../../../common/schemas";
-import { constantTimeCompare, convertRedirectableMobileScheme, isErr } from "../../../common/utils";
+
+import { convertRedirectableMobileScheme, isErr, timingSafeStringEqual } from "../../../common/utils";
 import { newClientType, newOAuthProvider, oauthProviderSchema } from "../../../domain/value-object";
 import { DrizzleService } from "../../../infrastructure/drizzle";
 import { OAuthProviderGateway } from "../../../interface-adapter/gateway/oauth-provider";
@@ -19,8 +19,15 @@ import { OAuthAccountRepository } from "../../../interface-adapter/repositories/
 import { SessionRepository } from "../../../interface-adapter/repositories/session";
 import { UserRepository } from "../../../interface-adapter/repositories/user";
 import { CookieManager } from "../../../modules/cookie";
-import { ElysiaWithEnv, RedirectResponse, RedirectResponseSchema } from "../../../modules/elysia-with-env";
-import { BadRequestException, ErrorResponseSchema, InternalServerErrorResponseSchema } from "../../../modules/error";
+import {
+	ElysiaWithEnv,
+	ErrorResponseSchema,
+	InternalServerErrorResponseSchema,
+	RedirectResponse,
+	RedirectResponseSchema,
+	ResponseTUnion,
+} from "../../../modules/elysia-with-env";
+import { BadRequestException } from "../../../modules/error";
 import { pathDetail } from "../../../modules/open-api";
 import { RateLimiterSchema, rateLimit } from "../../../modules/rate-limit";
 
@@ -98,10 +105,10 @@ export const OAuthSignupCallback = new ElysiaWithEnv()
 			const codeVerifier = cookieManager.getCookie(OAUTH_CODE_VERIFIER_COOKIE_NAME);
 			const redirectURI = cookieManager.getCookie(OAUTH_REDIRECT_URI_COOKIE_NAME);
 
-			if (!queryState || !constantTimeCompare(queryState, signedState)) {
+			if (!queryState || !timingSafeStringEqual(queryState, signedState)) {
 				throw new BadRequestException({
-					name: "INVALID_STATE",
-					message: "Invalid state",
+					code: "INVALID_OAUTH_STATE",
+					message: "Invalid OAuth state. Please try again.",
 				});
 			}
 
@@ -119,43 +126,55 @@ export const OAuthSignupCallback = new ElysiaWithEnv()
 			cookieManager.deleteCookie(OAUTH_REDIRECT_URI_COOKIE_NAME);
 
 			if (isErr(result)) {
-				if (
-					result.code === "INVALID_STATE" ||
-					result.code === "INVALID_REDIRECT_URL" ||
-					result.code === "CODE_NOT_FOUND"
-				) {
-					throw new BadRequestException({
-						code: result.code,
-					});
-				}
+				const { code } = result;
 
-				// Account Association Challenge Flow
-				if (result.code === "EMAIL_ALREADY_EXISTS_BUT_LINKABLE") {
-					const {
-						code,
-						value: { redirectURL, clientType, accountAssociationSessionToken, accountAssociationSession },
-					} = result;
+				switch (code) {
+					case "INVALID_OAUTH_STATE":
+						throw new BadRequestException({
+							code: code,
+							message: "Invalid OAuth state. Please try again.",
+						});
+					case "INVALID_REDIRECT_URL":
+						throw new BadRequestException({
+							code: code,
+							message: "Invalid redirect URL. Please check the URL and try again.",
+						});
+					case "OAUTH_CODE_MISSING":
+						throw new BadRequestException({
+							code: code,
+							message: "OAuth code is missing. Please try again.",
+						});
+					case "OAUTH_EMAIL_ALREADY_REGISTERED_BUT_LINKABLE": {
+						// Account Association Challenge Flow
+						const {
+							code: errorCode,
+							value: { redirectURL, clientType, accountAssociationSessionToken, accountAssociationSession },
+						} = result;
 
-					if (clientType === newClientType("mobile")) {
-						redirectURL.searchParams.set("account-association-session-token", accountAssociationSessionToken);
-						set.headers["referrer-policy"] = "strict-origin";
-						return RedirectResponse(convertRedirectableMobileScheme(redirectURL));
+						if (clientType === newClientType("mobile")) {
+							redirectURL.searchParams.set("account-association-session-token", accountAssociationSessionToken);
+							redirectURL.searchParams.set("error", errorCode);
+							set.headers["referrer-policy"] = "strict-origin";
+							return RedirectResponse(convertRedirectableMobileScheme(redirectURL));
+						}
+
+						cookieManager.setCookie(ACCOUNT_ASSOCIATION_SESSION_COOKIE_NAME, accountAssociationSessionToken, {
+							expires: accountAssociationSession.expiresAt,
+						});
+
+						redirectURL.searchParams.set("error", errorCode);
+						return RedirectResponse(redirectURL.toString());
 					}
-
-					cookieManager.setCookie(ACCOUNT_ASSOCIATION_SESSION_COOKIE_NAME, accountAssociationSessionToken, {
-						expires: accountAssociationSession.expiresAt,
-					});
-
-					redirectURL.searchParams.set("error", code);
-					return RedirectResponse(redirectURL.toString());
+					default: {
+						// If there is an error, add the error to the redirect URL and redirect
+						const {
+							code: errorCode,
+							value: { redirectURL },
+						} = result;
+						redirectURL.searchParams.set("error", errorCode);
+						return RedirectResponse(redirectURL.toString());
+					}
 				}
-
-				const {
-					code,
-					value: { redirectURL },
-				} = result;
-				redirectURL.searchParams.set("error", code);
-				return RedirectResponse(redirectURL.toString());
 			}
 
 			const { session, sessionToken, redirectURL, clientType } = result;
@@ -210,11 +229,10 @@ export const OAuthSignupCallback = new ElysiaWithEnv()
 			}),
 			response: {
 				302: RedirectResponseSchema,
-				400: FlattenUnion(
-					ErrorResponseSchema("INVALID_STATE"),
-					ErrorResponseSchema("INVALID_SIGNED_STATE"),
-					ErrorResponseSchema("FAILED_TO_DECODE_SIGNED_STATE"),
+				400: ResponseTUnion(
+					ErrorResponseSchema("INVALID_OAUTH_STATE"),
 					ErrorResponseSchema("INVALID_REDIRECT_URL"),
+					ErrorResponseSchema("OAUTH_CODE_MISSING"),
 				),
 				429: RateLimiterSchema.response[429],
 				500: InternalServerErrorResponseSchema,
@@ -226,11 +244,12 @@ export const OAuthSignupCallback = new ElysiaWithEnv()
 					"OAuth Signup Callback for the provider",
 					"##### **Error Query**",
 					"---",
-					"- `FAILED_TO_GET_ACCOUNT_INFO`",
-					"- `ACCOUNT_IS_ALREADY_USED`",
-					"- `ACCESS_DENIED`",
-					"- `PROVIDER_ERROR`",
-					"- `EMAIL_ALREADY_EXISTS_BUT_LINKABLE`",
+					"- **OAUTH_PROVIDER_UNAVAILABLE**",
+					"- **OAUTH_ACCESS_DENIED**",
+					"- **OAUTH_PROVIDER_ERROR**",
+					"- **OAUTH_ACCOUNT_NOT_FOUND**",
+					"- **OAUTH_ACCOUNT_NOT_FOUND_BUT_LINKABLE**",
+					"- **OAUTH_ACCOUNT_ALREADY_USED**",
 				],
 				tag: "Auth - OAuth",
 			}),
