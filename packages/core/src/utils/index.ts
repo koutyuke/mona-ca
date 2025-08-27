426fb2d62ee4f9b14b1041bc603389978f1eb905
koutyuke <gunji.kousuke08@gmail.com>
2025-08-27 11:35:20 +0900

    ✨ feat(core/utils/result): enhance Result type with Ok type and add ok utility function

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,4 +1,4 @@
 export { getAPIBaseURL, getMobileScheme, getWebBaseURL } from "./get-base-url";
 export { validateRedirectURL } from "./validate-redirect-url";
-export { err, isErr, type Err, type Result } from "./result";
+export { ok, err, isOk, isErr, type Err, type Ok, type Result } from "./result";
 export type { NewType, ToPrimitive } from "./new-type";
--- packages/core/src/utils/result.ts
+++ packages/core/src/utils/result.ts
@@ -1,10 +1,14 @@
 const IS_ERR = Symbol("isErr");
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export type Result<T, E extends Err<string, any>> = T | E;
+export type Result<T extends Ok<any>, E extends Err<string, any>> = T | E;
+
+export type Ok<T> = { [IS_ERR]: false; value: T };
 
 // biome-ignore lint/complexity/noBannedTypes: <explanation>
 export type Err<E extends string, D = {}> = { [IS_ERR]: true; code: E; value: D };
+
+export const ok = <T>(value: T): Ok<T> => ({ [IS_ERR]: false, value });
 
 // biome-ignore lint/complexity/noBannedTypes: <explanation>
 export const err = <E extends string, D = {}>(code: E, value: D = {} as D): Err<E, D> => ({
@@ -14,5 +18,9 @@ export const err = <E extends string, D = {}>(code: E, value: D = {} as D): Err<
 });
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-export const isErr = <T, E extends Err<string, any>>(result: Result<T, E>): result is E =>
+export const isOk = <T extends Ok<any>, E extends Err<string, any>>(result: Result<T, E>): result is T =>
+	typeof result === "object" && result !== null && (result as T)[IS_ERR] === false;
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export const isErr = <T extends Ok<any>, E extends Err<string, any>>(result: Result<T, E>): result is E =>
 	typeof result === "object" && result !== null && (result as E)[IS_ERR] === true;
