b6284c193c2c07b789672685b82107163ec336db
koutyuke <gunji.kousuke08@gmail.com>
2025-10-01 23:53:34 +0900

    🎈 feat(catalog/app): add package name and update font configuration for better integration

--- apps/catalog/src/app/_layout.tsx
+++ apps/catalog/src/app/_layout.tsx
@@ -1,4 +1,3 @@
-import { KiwiMaru_300Light, KiwiMaru_400Regular, KiwiMaru_500Medium, useFonts } from "@expo-google-fonts/kiwi-maru";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { Provider as JotaiProvider } from "jotai";
@@ -7,40 +6,26 @@ import { ThemeProvider } from "../../../mobile/src/layers/app/providers";
 
 import "react-native-reanimated";
 import "./global.css";
+import { View } from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
-	const [fontsLoaded, fontError] = useFonts({
-		KiwiMaru_300Light,
-		KiwiMaru_400Regular,
-		KiwiMaru_500Medium,
-	});
-
 	useEffect(() => {
-		if (fontsLoaded || fontError) {
-			SplashScreen.hide();
-		}
-	}, [fontsLoaded, fontError]);
-
-	if (!fontsLoaded) {
-		return null;
-	}
-
-	if (fontError) {
-		console.error(fontError);
-		return null;
-	}
+		SplashScreen.hide();
+	}, []);
 
 	return (
-		<JotaiProvider>
-			<ThemeProvider>
-				<Stack>
-					<Stack.Screen name="index" options={{ headerShown: false }} />
-				</Stack>
-			</ThemeProvider>
-		</JotaiProvider>
+		<View className="ios:ios android:android h-full w-full">
+			<JotaiProvider>
+				<ThemeProvider>
+					<Stack>
+						<Stack.Screen name="index" options={{ headerShown: false }} />
+					</Stack>
+				</ThemeProvider>
+			</JotaiProvider>
+		</View>
 	);
 };
 
