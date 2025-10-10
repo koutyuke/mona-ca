94244c3e541c03f81b0135e0095c51a08a1c3717
koutyuke <gunji.kousuke08@gmail.com>
2025-10-10 13:16:43 +0900

    🪦 feat(api/application): remove session-secret service

--- apps/api/src/application/services/session/index.ts
+++
@@ -1,3 +0,0 @@
-export type { ISessionSecretService } from "./interfaces/session-secret.service.interface";
-export { SessionSecretService } from "./session-secret.service";
-export { separateSessionTokenToIdAndSecret, createSessionToken } from "./utils";
