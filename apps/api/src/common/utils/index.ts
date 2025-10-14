50251a1304e2b581b1c5b6b85db5fe52c8929ec7
koutyuke <gunji.kousuke08@gmail.com>
2025-10-14 20:15:33 +0900

    🪦 feat(api/common/utils): remove result utility functions

--- apps/api/src/common/utils/index.ts
+++ apps/api/src/common/utils/index.ts
@@ -6,9 +6,7 @@ export { timingSafeStringEqual } from "./timing-safe-string-equal";
 export { convertRedirectableMobileScheme } from "./convert-redirectable-mobile-scheme";
 export { getIP } from "./get-ip";
 export { readBearerToken } from "./read-bearer-token";
-export { err, isErr } from "./result";
 export { TimeSpan } from "./time-span";
 export { ulid } from "./ulid";
 
-export type { Err, Result } from "./result";
 export type { TimeSpanUnit } from "./time-span";
