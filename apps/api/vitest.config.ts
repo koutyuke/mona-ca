082e9ead8addd491b30e111d233d1c6740125b04
koutyuke <gunji.kousuke08@gmail.com>
2025-10-20 00:26:26 +0900

    🎈 feat(api): update test configuration and add migration application script for improved testing setup

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -11,7 +11,7 @@ export default defineWorkersConfig(async () => {
 		resolve: {},
 		test: {
 			globals: true,
-			setupFiles: ["./src/tests/apply-migrations.ts"],
+			setupFiles: ["./tests/apply-migrations.ts"],
 			poolOptions: {
 				workers: {
 					singleWorker: true,
