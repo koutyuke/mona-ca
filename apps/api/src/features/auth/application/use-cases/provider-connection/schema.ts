6c1b2dd5f747638554d532383ade3f32743644da
koutyuke <gunji.kousuke08@gmail.com>
2025-12-15 15:52:25 +0900

    ♻️ refactor(auth): remove obsolete provider connection use cases and schema to streamline authentication flow

--- apps/api/src/features/auth/application/use-cases/provider-connection/schema.ts
+++
@@ -1,7 +0,0 @@
-import { t } from "elysia";
-import { clientPlatformSchema } from "../../../../../core/domain/value-objects";
-
-export const providerConnectionStateSchema = t.Object({
-	uid: t.String(),
-	client: clientPlatformSchema,
-});
