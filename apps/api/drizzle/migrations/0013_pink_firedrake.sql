733c1e911ba1a82379efe4dc1f877f5ce33c8d48
koutyuke <gunji.kousuke08@gmail.com>
2025-12-24 12:06:57 +0900

    🎈 improve(auth): combine database migration files in this branch

--- apps/api/drizzle/migrations/0013_pink_firedrake.sql
+++ apps/api/drizzle/migrations/0013_pink_firedrake.sql
@@ -1,3 +1,30 @@
+CREATE TABLE `account_link_requests` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`secret_hash` blob NOT NULL,
+	`code` text,
+	`email` text NOT NULL,
+	`provider` text NOT NULL,
+	`provider_user_id` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE INDEX `idx_account_link_requests__expires_at` ON `account_link_requests` (`expires_at`);--> statement-breakpoint
+CREATE UNIQUE INDEX `unq_account_link_requests__provider_user` ON `account_link_requests` (`provider`,`user_id`);--> statement-breakpoint
+CREATE TABLE `email_verification_requests` (
+	`id` text PRIMARY KEY NOT NULL,
+	`email` text NOT NULL,
+	`user_id` text NOT NULL,
+	`code` text NOT NULL,
+	`secret_hash` blob NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `email_verification_requests_user_id_unique` ON `email_verification_requests` (`user_id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `email_verification_requests_code_unique` ON `email_verification_requests` (`code`);--> statement-breakpoint
+CREATE UNIQUE INDEX `unq_email_verification_requests__user_id_email` ON `email_verification_requests` (`user_id`,`email`);--> statement-breakpoint
 CREATE TABLE `provider_accounts` (
 	`provider` text NOT NULL,
 	`provider_user_id` text NOT NULL,
@@ -8,24 +35,21 @@ CREATE TABLE `provider_accounts` (
 --> statement-breakpoint
 CREATE UNIQUE INDEX `unq_provider_accounts__provider_account` ON `provider_accounts` (`provider`,`provider_user_id`);--> statement-breakpoint
 CREATE UNIQUE INDEX `unq_provider_accounts__provider_user` ON `provider_accounts` (`provider`,`user_id`);--> statement-breakpoint
-CREATE TABLE `provider_connection_tickets` (
+CREATE TABLE `provider_link_requests` (
 	`id` text PRIMARY KEY NOT NULL,
 	`user_id` text NOT NULL,
+	`provider` text NOT NULL,
 	`secret_hash` blob NOT NULL,
 	`expires_at` integer NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-CREATE UNIQUE INDEX `provider_connection_tickets_user_id_unique` ON `provider_connection_tickets` (`user_id`);--> statement-breakpoint
-CREATE INDEX `idx_provider_connection_tickets__expires_at` ON `provider_connection_tickets` (`expires_at`);--> statement-breakpoint
+CREATE UNIQUE INDEX `provider_link_requests_user_id_unique` ON `provider_link_requests` (`user_id`);--> statement-breakpoint
+CREATE INDEX `idx_provider_link_requests__expires_at` ON `provider_link_requests` (`expires_at`);--> statement-breakpoint
 DROP TABLE `account_association_sessions`;--> statement-breakpoint
+DROP TABLE `account_link_sessions`;--> statement-breakpoint
+DROP TABLE `email_verification_sessions`;--> statement-breakpoint
 DROP TABLE `external_identities`;--> statement-breakpoint
-DROP INDEX `account_link_sessions_user_id_unique`;--> statement-breakpoint
-ALTER TABLE `account_link_sessions` ADD `code` text;--> statement-breakpoint
-ALTER TABLE `account_link_sessions` ADD `email` text NOT NULL;--> statement-breakpoint
-ALTER TABLE `account_link_sessions` ADD `provider` text NOT NULL;--> statement-breakpoint
-ALTER TABLE `account_link_sessions` ADD `provider_user_id` text NOT NULL;--> statement-breakpoint
-CREATE UNIQUE INDEX `unq_account_link_sessions__provider_user` ON `account_link_sessions` (`provider`,`user_id`);--> statement-breakpoint
 PRAGMA foreign_keys=OFF;--> statement-breakpoint
 CREATE TABLE `__new_users` (
 	`id` text PRIMARY KEY NOT NULL,
