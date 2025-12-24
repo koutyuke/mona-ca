ff9c85da2a43a77fb6615db9fb24b20a55e8663f
koutyuke <gunji.kousuke08@gmail.com>
2025-12-24 11:47:18 +0900

    🎈 improve(auth): update OpenAPI tags to include 'Me - Provider Link' and remove 'Auth - Account Association' for better clarity in authentication endpoints

--- apps/api/src/plugins/openapi/tag.ts
+++ apps/api/src/plugins/openapi/tag.ts
@@ -5,10 +5,10 @@ export const tags = [
 	{ name: "Auth", description: "Endpoints for authentication and authorization" },
 	{ name: "Auth - Federated Auth", description: "Endpoints for authentication and authorization with External Auth" },
 	{ name: "Auth - Account Link", description: "Endpoints for account link" },
-	{ name: "Auth - Account Association", description: "Endpoints for account association" },
 	{ name: "Auth - Email Verification", description: "Endpoints for email verification" },
 	{ name: "Auth - Password Reset", description: "Endpoints for password reset" },
 	{ name: "Me", description: "Endpoints for the current user" },
+	{ name: "Me - Provider Link", description: "Endpoints for provider link" },
 	{ name: "User", description: "Endpoints for the other users" },
 ] as const satisfies OpenAPIV3.TagObject[];
 
