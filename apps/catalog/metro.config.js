c8b126fddcb9579ca098df56b141d716fbfa4775
koutyuke <gunji.kousuke08@gmail.com>
2025-12-31 17:54:35 +0900

    🔧 chore(dependencies): update Storybook and related packages to version 10.1.11, adjust Storybook configuration to enable based on environment variable, and improve compatibility with nativewind

--- apps/catalog/metro.config.js
+++ apps/catalog/metro.config.js
@@ -1,7 +1,8 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const withStorybook = require("@storybook/react-native/metro/withStorybook");
+const { withStorybook } = require("@storybook/react-native/metro/withStorybook");
+const path = require("node:path");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
@@ -28,7 +29,8 @@ const withNativeWindConfig = withNativeWind(config, {
 });
 
 const withStorybookConfig = withStorybook(withNativeWindConfig, {
-	enabled: true,
+	enabled: process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === "true",
+	configPath: path.resolve(__dirname, "./.rnstorybook"),
 });
 
 module.exports = withStorybookConfig;
